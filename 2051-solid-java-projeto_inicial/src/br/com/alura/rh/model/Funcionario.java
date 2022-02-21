8f93c2cc310c151caf4b83d4c2527e53ea03d64a
CaioMitsuo <caio.lima@sankhya.com.br>
2022-02-21 14:55:09 -0300

    Extraindo a lógica de reajuste salarial

--- 2051-solid-java-projeto_inicial/src/br/com/alura/rh/model/Funcionario.java
+++ 2051-solid-java-projeto_inicial/src/br/com/alura/rh/model/Funcionario.java
@@ -1,74 +1,70 @@
 package br.com.alura.rh.model;
 
-import br.com.alura.rh.ValidacaoException;
-
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.time.LocalDate;
 
 
 public class Funcionario {
 
-	private String nome;
-	private String cpf;
-	private Cargo cargo;
-	private BigDecimal salario;
-	private LocalDate dataUltimoReajuste;
-	//Construtor
-	public Funcionario(String nome, String cpf, Cargo cargo, BigDecimal salario) {
-		this.nome = nome;
-		this.cpf = cpf;
-		this.cargo = cargo;
-		this.salario = salario;
-	}
-	//Metodo para Reajustar o Salario do Funcionario
-	public void reajustarSalario(BigDecimal aumento) {
-		BigDecimal percentualReajuste = aumento.divide(salario, RoundingMode.HALF_UP);
-		if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
-			throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
-		}
-		this.salario = this.salario.add(aumento);
-		this.dataUltimoReajuste = LocalDate.now();
-	}
-	//getter e setters
-	public String getNome() {
-		return nome;
-	}
+    private String nome;
+    private String cpf;
+    private Cargo cargo;
+    private BigDecimal salario;
+    private LocalDate dataUltimoReajuste;
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    //Construtor
+    public Funcionario(String nome, String cpf, Cargo cargo, BigDecimal salario) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.cargo = cargo;
+        this.salario = salario;
+    }
 
-	public String getCpf() {
-		return cpf;
-	}
+    //Metodo para Reajustar o Salario do Funcionario
+    public void atualizarSalario(BigDecimal novoSalario) {
+        this.salario = novoSalario;
+        this.dataUltimoReajuste = LocalDate.now();
+    }
 
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
+    //getter e setters
+    public String getNome() {
+        return nome;
+    }
 
-	public Cargo getCargo() {
-		return cargo;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	public void setCargo(Cargo cargo) {
-		this.cargo = cargo;
-	}
+    public String getCpf() {
+        return cpf;
+    }
 
-	public BigDecimal getSalario() {
-		return salario;
-	}
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 
-	public void setSalario(BigDecimal salario) {
-		this.salario = salario;
-	}
+    public Cargo getCargo() {
+        return cargo;
+    }
 
-	public LocalDate getDataUltimoReajuste() {
-		return dataUltimoReajuste;
-	}
+    public void setCargo(Cargo cargo) {
+        this.cargo = cargo;
+    }
 
-	public void setDataUltimoReajuste(LocalDate dataUltimoReajuste) {
-		this.dataUltimoReajuste = dataUltimoReajuste;
-	}
+    public BigDecimal getSalario() {
+        return salario;
+    }
+
+    public void setSalario(BigDecimal salario) {
+        this.salario = salario;
+    }
+
+    public LocalDate getDataUltimoReajuste() {
+        return dataUltimoReajuste;
+    }
+
+    public void setDataUltimoReajuste(LocalDate dataUltimoReajuste) {
+        this.dataUltimoReajuste = dataUltimoReajuste;
+    }
 
 }
